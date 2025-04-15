e32b0c721b42fe1262b875edd3b4e6d962156bb0
Terra SARR <samba.co93@gmail.com>
2025-04-15 17:34:59 +0000

    my portofolio page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Work />
+      </main>
+    </>
+  );
+};
+
+export default App;
