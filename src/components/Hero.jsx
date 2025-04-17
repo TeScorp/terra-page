c2d99c85c4719e417060b7a53ab4e710baf3981d
Terra SARR <samba.co93@gmail.com>
2025-04-17 22:22:34 +0000

    Mise à jour des fichiers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             </div>
           </div>
 
-          <h2 className="headline- 1 max-w-[15ch] sm:max-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
             Building Scalable Modern Websites for the Future
           </h2>
 
